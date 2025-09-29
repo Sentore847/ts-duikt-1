cd7abd0d532ec3ed2d1e7a4a8c248a164d79e4e4
Dmytro Grygorchuk <dmytro.grygorchuk@amedia.ua>
2025-09-29 18:27:22 +0300

    Налаштовано TypeScript проєкт

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let studentName = "Dmytro";
+let studentAge = 20;
+let isDuiktStudent = true;
+console.log(`Ім'я: ${studentName}, Вік: ${studentAge}, Студент: ${isDuiktStudent}`);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let studentName: string = "Dmytro";
+let studentAge: number = 20;
+let isDuiktStudent: boolean = true;
+
+console.log(`Ім'я: ${studentName}, Вік: ${studentAge}, Студент: ${isDuiktStudent}`);
